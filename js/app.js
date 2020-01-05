31bb4f4580cd779cca005002624e9a4692bf9695
Piyawat Supasri <piyawat.su54@gmail.com>
2020-01-05 15:14:15 +0700

    final

--- js/app.js
+++ js/app.js
@@ -28,13 +28,16 @@ function pad(n) {
 }
 
 // Animation script
-// gsap.set('.fireworks-1, .fireworks-2, .fireworks-3', { opacity: 0 });
 gsap.set('.firework', {opacity: 0, y: 50});
+gsap.set('#new-year-text path:nth-child(1)', { css: { strokeDashoffset: '500' }});
+gsap.set('#new-year-text path:nth-child(2)', { css: { strokeDashoffset: '930' }});
 
 function playAnimation() {
   let tl = gsap.timeline();
 
-  tl.to(".time-counter", {duration: 0.3, opacity: 0})
-    .to(".firework", { duration: 2, opacity: 1, y: 0, stagger: 0.2, ease: 'power4.outs' })
-    .to(".firework", { duration: 2, opacity: 0.7, y: 0, ease: 'power4.outs' })
+  tl.to('.time-counter', {duration: 0.3, opacity: 0})
+    .to('.firework', { duration: 2, opacity: 1, y: 0, stagger: 0.2, ease: 'power4.outs' })
+    .to('.firework', { duration: 1, opacity: 0.3, y: 0, ease: 'power4.outs' })
+    .to('#new-year-text path', { duration: 3, css: { strokeDashoffset: '0' }, ease: 'power4.outs', delay: '-5' })
+    .to('#new-year-text path', { duration: 2, css: { fill: 'rgba(255,255,255,1)' }, ease: 'power4.outs', delay: '-2' })
 }
